bc4d701aa424380672774f7569b909c85db8ae93
ngocAnh1999 <43126165+ngocAnh1999@users.noreply.github.com>
2018-09-25 22:06:48 +0700

    Add files via upload

---
+++ Hemattroii.java
@@ -0,0 +1,14 @@
+package Lab03;
+
+public class Hemattroii {
+	private String hanhTinh;
+	private String thienThach;
+	public void setHanhTinh(String hanhtinh) { this.hanhTinh = hanhtinh; }
+	public void setThienThach(String thienthach) { this.thienThach = thienthach; }
+	public String getHanhTinh() { return this.hanhTinh;}
+	public String getThienThach() { return this.thienThach;}
+	public Hemattroii() {
+		this.hanhTinh = "Trai Dat";
+		this.thienThach = "sao bang";
+	}
+}
