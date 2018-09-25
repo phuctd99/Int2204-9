b279f2cdf34bd5fff58f9a37f0cdd7e4e536f625
Nguyendacphong09 <43370580+Nguyendacphong09@users.noreply.github.com>
2018-09-25 23:52:12 +0700

    Add files via upload

---
+++ Group2/17020953/lab03/Laptop.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai_3;
+
+/**
+ *
+ * @author CCNE
+ */
+public class Laptop {
+    private String name, manufacturer , price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
---
+++ Group2/17020953/lab03/Teacher.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bai_3;
+
+/**
+ *
+ * @author CCNE
+ */
+public class Teacher {
+    private String name,id,  year, school;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+    
+    
+}
