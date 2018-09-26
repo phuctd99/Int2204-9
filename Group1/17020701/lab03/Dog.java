54dda5eda8f1f51efe89c2d0d037069b040b8f86
anhducc13 <42963466+anhducc13@users.noreply.github.com>
2018-09-26 10:23:18 +0700

    ductt

---
+++ Group1/17020701/lab03/Dog.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3;
+
+public class Dog {
+    private String loaiCho;
+    private boolean giong;
+    private String mauLong;
+    private String ten;
+
+    public String getLoaiCho() {
+        return loaiCho;
+    }
+
+    public void setLoaiCho(String loaiCho) {
+        this.loaiCho = loaiCho;
+    }
+
+    public boolean isGiong() {
+        return giong;
+    }
+
+    public void setGiong(boolean giong) {
+        this.giong = giong;
+    }
+
+    public String getMauLong() {
+        return mauLong;
+    }
+
+    public void setMauLong(String mauLong) {
+        this.mauLong = mauLong;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+    
+    
+}
---
+++ Group1/17020701/lab03/Sach.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week3;
+
+/**
+ *
+ * @author Admin
+ */
+public class Sach {
+    private String tenSach;
+    private String tenTacGia;
+    private int soTrang;
+    private int namSx;
+
+    public String getTenSach() {
+        return tenSach;
+    }
+
+    public void setTenSach(String tenSach) {
+        this.tenSach = tenSach;
+    }
+
+    public String getTenTacGia() {
+        return tenTacGia;
+    }
+
+    public void setTenTacGia(String tenTacGia) {
+        this.tenTacGia = tenTacGia;
+    }
+
+    public int getSoTrang() {
+        return soTrang;
+    }
+
+    public void setSoTrang(int soTrang) {
+        this.soTrang = soTrang;
+    }
+
+    public int getNamSx() {
+        return namSx;
+    }
+
+    public void setNamSx(int namSx) {
+        this.namSx = namSx;
+    }
+    
+    
+}
