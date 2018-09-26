c5b63dcea0159c00a012c24b4102654f497b61f8
Đôn <43126168+lequydon1236987@users.noreply.github.com>
2018-09-26 15:05:53 +0700

    Add files via upload

---
+++ Group1/17020685/lab03/Bai3.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai.pkg3;
+
+/**
+ *
+ * @author HP
+ */
+public class Bai3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    static class school{
+        private String name, date;
+        public String getName(){
+            return name;
+        }
+        public String getDate(){
+            return date;
+        }
+        public void setName(String name) {
+            this.name=name;
+        }
+        public void setDate(String date) {
+            this.date=date;
+        }
+    }
+    static class classs{
+        private String name;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+    }
+    static class dog{
+        private String name, date;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name) {
+            this.name=name;
+        }
+        public String getDate(){
+            return date;
+        }
+        public void setDate(String date) {
+            this.date=date;
+        }
+    }
+    static class cat{
+        private String name, date;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+        public String getDate(){
+            return name;
+        }
+        public void setDate(String date ){
+            this.date=date;
+        }
+    }
+    static class student {
+        private String name, id, group;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+        public String getId(){
+            return id;
+        } 
+        public void setId(String id){
+            this.id=id;
+        }
+        public String getGroup(){
+            return group;
+        }
+        public void setGroup(String group) {
+            this.group=group;
+        }
+    }
+    static class teacher{
+        private String name, date,age;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+        public String getDate(){
+            return date;
+        }
+        public void setDate(String date){
+            this.date=date;
+        }
+        public String getAge(){
+            return age;
+        }
+        public void setAge(String age){
+            this.age=age;
+        }
+    }
+    static class child{
+        private String name, date,age;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+        public String getDate(){
+            return date;
+        }
+        public void setDate(String date){
+            this.date=date;
+        }
+        public String getAge(){
+            return age;
+        }
+        public void setAge(String age){
+            this.age=age;
+        }
+        
+        
+    }
+    static class dad{
+        private String name, date;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+        public String getDate(){
+            return date;
+        }
+        public void setDate(String date){
+            this.date=date;
+        }
+    }
+    static class mom{
+        private String name, date,age;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+        public String getDate(){
+            return date;
+        }
+        public void setDate(String date){
+            this.date=date;
+        }
+        public String getAge(){
+            return age;
+        }
+        public void setAge(String age){
+            this.age=age;
+        }
+    }
+    static class mysister{
+        private String name, date;
+        public String getName(){
+            return name;
+        }
+        public void setName(String name){
+            this.name=name;
+        }
+        public String getDate(){
+            return date;
+        }
+        public void setDate(String date){
+            this.date=date;
+        }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
