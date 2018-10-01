9ed9b98284e2dfeef70ac1ec8b49c62742d2e3c0
huyengocao <43350545+huyengocao@users.noreply.github.com>
2018-10-01 10:08:06 +0700

    Add files via upload

---
+++ Group1/17020807/lab04/Bai_tuan_4.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai_tuan_4;
+import java.util.*;
+/**
+ *
+ * @author HP
+ */
+public class Bai_tuan_4{
+
+    /**
+     * @param args the command line arguments
+     * @return 
+     */
+    static int SoMax(int a, int b) {
+        if (a > b){
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    static int GTNN(int[] arr){
+        Arrays.sort(arr);
+        return arr[0];      
+    }
+    static String BMI(double ccao, double cnang){
+        double bmi = cnang/(ccao*ccao);
+        String tmp = null;
+        if (bmi < 18.5){
+            tmp = "Thieu can";
+        }
+        else if (bmi >= 18.5 && bmi <= 22.99){
+            tmp = "Binh thuong";
+        }
+        else if (bmi >= 23 && bmi <= 24.99) 
+        {
+            tmp = "Thua can";
+        }
+        else if (bmi >= 25){
+            tmp = "Beo phi";
+        }   
+        return tmp;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
