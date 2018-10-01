fbdc0f88c135180c46625348ab96aa24bcb5539c
Nguyễn Tuấn Linh <43125994+1m93@users.noreply.github.com>
2018-10-01 09:48:37 +0700

    Add files via upload

---
+++ Group1/17020859/lab04/MaxOf2.java
@@ -0,0 +1,11 @@
+public class MaxOf2 {
+
+    public static int MaxOf2(int a, int b) {
+        if (a > b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+}
