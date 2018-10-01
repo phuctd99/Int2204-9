8bf66caf930c0cc418a2e72f26c9e78431193428
tungvv318 <43350201+tungvv318@users.noreply.github.com>
2018-10-01 12:07:39 +0700

    Update Week4Test.java

--- Group2/17021115/lab04/Week4Test.java
+++ Group2/17021115/lab04/Week4Test.java
@@ -1,39 +1,90 @@
 package OopWeek4;
 
-public class Week4 {
-	
-	public static int timMax(int a, int b) {
-		if(a > b) {
-			return a;
-		}
-		return b;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Week4Test {
+
+	@Test
+	void testTimMax1() {
+		int a = Week4.timMax(4, 4);
+		assertEquals(4,a);
 	}
-	
-	public static int timMinCuaMang(int []mangSoNguyen) {
-		int min = mangSoNguyen[0];
-		for(int i=1; i<mangSoNguyen.length; i++) {
-			if(min > mangSoNguyen[i]) {
-				min = mangSoNguyen[i];
-			}
-		}
-		return min;
+	@Test
+	void testTimMax2() {
+		int b = Week4.timMax(3, 3);
+		assertEquals(3,b);
 	}
-	
-	public static String tinhChiSoBMI(double canNang, double chieuCao) {
-		double BMI = canNang / (chieuCao*chieuCao);
-		String ketQua = "";
-		if(BMI < 18.5) {
-			ketQua = "Thiếu cân";
-		}
-		if(BMI >= 18.5 && BMI <= 22.99) {
-			ketQua = "Bình thường";
-		}
-		if(BMI >= 23 && BMI <= 25) {
-			ketQua = "Thừa cân";
-		}
-		if(BMI > 25) {
-			ketQua = "Béo phì";
-		}
-		return ketQua;
+	@Test
+	void testTimMax3() {
+		int c = Week4.timMax(-8, 4);
+		assertEquals(4,c);
 	}
+	@Test
+	void testTimMax4() {
+		int d = Week4.timMax(1, -2);
+		assertEquals(1,d);
+	}
+	@Test
+	void testTimMax5() {
+		int e = Week4.timMax(3, 7);
+		assertEquals(7,e);
+	}
+	@Test
+	void testTimMinCuaMang1() {
+		int[] arr1 = {4,3,7,1,0};
+		int a = Week4.timMinCuaMang(arr1);
+		assertEquals(0,a);
+	}
+	@Test
+	void testTimMinCuaMang2() {
+		int[] arr2 = {1,2,3,4,5,6,7,8,9,10};
+		int b = Week4.timMinCuaMang(arr2);
+		assertEquals(1,b);
+	}
+	@Test
+	void testTimMinCuaMang3() {
+		int[] arr3 = {-4,3,1,1,-4,10,16,0};
+		int c = Week4.timMinCuaMang(arr3);
+		assertEquals(-4,c);
+	}
+	@Test
+	void testTimMinCuaMang4() {
+		int[] arr4 = {-10,-9,-8,-7,-6,-5,-11};
+		int d = Week4.timMinCuaMang(arr4);
+		assertEquals(-11,d);
+	}
+	@Test
+	void testTimMinCuaMang5() {
+		int[] arr5 = {0,1,3,5,7,9,-9,-7,-5,-3,-1,0};
+		int e = Week4.timMinCuaMang(arr5);
+		assertEquals(-9,e);
+	}
+	@Test
+	void testBMI1() {
+		String a = Week4.tinhChiSoBMI(29.4,1.4);
+		assertEquals("Thiếu cân",a);
+	}
+	@Test
+	void testBMI2() {
+		String b = Week4.tinhChiSoBMI(51.2,1.6);
+		assertEquals("Bình thường",b);
+	}
+	@Test
+	void testBMI3() {
+		String c = Week4.tinhChiSoBMI(54.0,1.5);
+		assertEquals("Thừa cân",c);
+	}
+	@Test
+	void testBMI4() {
+		String d = Week4.tinhChiSoBMI(86.7,1.7);
+		assertEquals("Béo phì",d);
+	}
+	@Test
+	void testBMI5() {
+		String e = Week4.tinhChiSoBMI(40.5,1.5);
+		assertEquals("Thiếu cân",e);
+	}
+
 }
