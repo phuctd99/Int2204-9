a36d81d766d6c2202e1efd1d011c446801046412
hoangkimminh <41377837+hoangkimminh@users.noreply.github.com>
2018-10-09 20:21:11 +0700

    Add files via upload

---
+++ Group2/17020891/lab05/Week4_ex1/CamCaoPhong.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class CamCaoPhong extends QuaCam {
+    //constructor
+    public CamCaoPhong(double khoiLuong, String nguonGoc, String ngayNhap) {
+        super(khoiLuong, nguonGoc, ngayNhap);
+        this.setTen("Cam Cao Phong");
+    }
+}
