4a4573cfa17b12c9f48dd1c4b56e5acf9dcf8573
hoangdinhbn <43349198+hoangdinhbn@users.noreply.github.com>
2018-10-15 21:05:28 +0700

    Add files via upload

---
+++ Group1/17020684/lab05/CamSanh.java
@@ -0,0 +1,57 @@
+
+package lab5;
+
+public class CamSanh extends QuaCam{
+    protected String mausac="xanh";
+    private int soluong;
+    private String ngaynhap;
+    private String nguongoc;
+    private double giaban;
+    
+    public CamSanh(){
+    }
+    public CamSanh( double giaban, String nguongoc, String ngaynhap, int soluong) {
+//        super( giaban , nguongoc , ngaynhap , soluong);
+    }
+    
+    public String getMausac() {
+        return mausac;
+    }
+
+    public void setMausac(String mausac) {
+        this.mausac = mausac;
+    }
+
+    public double getGiaban() {
+        return giaban;
+    }
+
+    public void setGiaban(double giaban) {
+        this.giaban = giaban;
+    }
+
+    public String getNguongoc() {
+        return nguongoc;
+    }
+
+    public void setNguongoc(String nguongoc) {
+        this.nguongoc = nguongoc;
+    }
+
+    public String getNgaynhap() {
+        return ngaynhap;
+    }
+
+    public void setNgaynhap(String ngaynhap) {
+        this.ngaynhap = ngaynhap;
+    }
+
+    public int getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+    
+}
---
+++ Group1/17020684/lab05/QuaTao.java
@@ -0,0 +1,46 @@
+
+package lab5;
+
+public class QuaTao extends HoaQua {
+    
+    
+    public QuaTao() {
+    }
+    public QuaTao(String Dongot, double giaban, String nguongoc, String ngaynhap, int soluong) {
+        super(giaban, nguongoc, ngaynhap, soluong);
+       
+    }
+
+    public double getGiaban() {
+        return giaban;
+    }
+
+    public void setGiaban(double giaban) {
+        this.giaban = giaban;
+    }
+
+    public String getNguongoc() {
+        return nguongoc;
+    }
+
+    public void setNguongoc(String nguongoc) {
+        this.nguongoc = nguongoc;
+    }
+
+    public String getNgaynhap() {
+        return ngaynhap;
+    }
+
+    public void setNgaynhap(String ngaynhap) {
+        this.ngaynhap = ngaynhap;
+    }
+
+    public int getSoluong() {
+        return soluong;
+    }
+
+    public void setSoluong(int soluong) {
+        this.soluong = soluong;
+    }
+    
+}
