bcf1312f93dce45501af6474d076f38432d3ddc7
huyengocao <43350545+huyengocao@users.noreply.github.com>
2018-10-16 23:46:35 +0700

    Add files via upload

---
+++ Group1/17020807/lab06/Lab06/Triangle.java
@@ -0,0 +1,37 @@
+public class Triangle extends Shape {
+
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle(String address, double size, boolean movable, boolean filled, double side1, double side2, double side3) {
+        super(address, size, movable, filled);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+}
