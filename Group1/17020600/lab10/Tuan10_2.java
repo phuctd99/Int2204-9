64d7013c967a57d18e33adaf578f9fdac4b06a3a
bao nguyen <43126083+baonguyen21021999@users.noreply.github.com>
2018-11-13 20:39:14 +0700

    Add files via upload

---
+++ Group1/17020600/lab10/Tuan10_2.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tuan10_2;
+
+import java.util.Random;
+
+
+
+/**
+ *
+ * @author pc
+ */
+public class Tuan10_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void bubblesort (double []a, int length){
+        int n = a.length;
+        for(int i = 0; i < n - 1; i++){
+            for(int j = 0; j <n - i - 1; j++){
+                if(a[j] > a[j+1]){
+                    double tmp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = tmp;
+                }
+            }
+        }
+        for(int i = 0; i<n ; i++){
+            System.out.println(a[i]);
+        }
+    }
+    public static void main(String[] args) {
+        try{
+        int n = 1000;
+        Random rd = new Random();
+        double []a = new double[n];
+        for(int i = 0; i < n; i++){
+            a[i] = rd.nextDouble();
+        }
+        bubblesort(a,n);
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("tran so");
+        }
+    }
+    
+}
